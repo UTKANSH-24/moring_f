a46cf9102a8042f1834d582a685aec6a09975a78
Kunal Kumar <kunal23kmr@gmail.com>
2024-02-16 13:15:39 +0530

    Schedule heading.

--- src/USER/schedule/Horizontaltab.js
+++ src/USER/schedule/Horizontaltab.js
@@ -15,19 +15,23 @@ export default function LabTabs() {
   };
 
   return (
-    <Box sx={{ width: '80%',margin:'auto', typography: 'body1',backgroundColor:'' }}>
-      <TabContext value={value}>
-        <Box sx={{   }}>
-          <TabList onChange={handleChange} sx={{ width: '50%',margin:'auto', typography: 'body1',backgroundColor:'' }} >
-            <Tab sx={{ width: '10%',margin:'auto',borderRadius:'5px', fontSize:'12px',typography: '',backgroundColor:'pink', height:15}}label="Item One" value="1" />
-            <Tab sx={{ width: '10%',margin:'auto',borderRadius:'5px', fontSize:'12px',typography: '',backgroundColor:'pink',height:15}}label="Item Two" value="2" />
-            <Tab style={{borderRadius:'5px'}} sx={{ width: '10%',margin:'auto', fontSize:'12px',typography: '',backgroundColor:'pink',height:15 }}label="Item Three" value="3" />
-          </TabList>
-        </Box>
-        <TabPanel value="1"><Schedule1/></TabPanel>
-        <TabPanel value="2"><Schedule2/></TabPanel>
-        <TabPanel value="3"><Schedule3/></TabPanel>
-      </TabContext>
-    </Box>
+    <div><h2 className="titl" style={{ textAlign: 'center', margin: '90px 0px 30px 0', fontSize: '3.6rem', zIndex: 100 }}>
+      Schedule
+    </h2>
+      <Box sx={{ width: '80%', margin: 'auto', typography: 'body1', backgroundColor: '' }}>
+        <TabContext value={value}>
+          <Box sx={{}}>
+            <TabList onChange={handleChange} sx={{ width: '50%', margin: 'auto', typography: 'body1', backgroundColor: '' }} >
+              <Tab sx={{ width: '10%', margin: 'auto', borderRadius: '5px', fontSize: '12px', typography: '', backgroundColor: 'pink', height: 15 }} label="Item One" value="1" />
+              <Tab sx={{ width: '10%', margin: 'auto', borderRadius: '5px', fontSize: '12px', typography: '', backgroundColor: 'pink', height: 15 }} label="Item Two" value="2" />
+              <Tab style={{ borderRadius: '5px' }} sx={{ width: '10%', margin: 'auto', fontSize: '12px', typography: '', backgroundColor: 'pink', height: 15 }} label="Item Three" value="3" />
+            </TabList>
+          </Box>
+          <TabPanel value="1"><Schedule1 /></TabPanel>
+          <TabPanel value="2"><Schedule2 /></TabPanel>
+          <TabPanel value="3"><Schedule3 /></TabPanel>
+        </TabContext>
+      </Box>
+    </div>
   );
 }
