933c341c10a8fd933e1d372a5edbd6466a498aa1
ABHISHEK KUMAR <100033618+abhishek110803@users.noreply.github.com>
2024-02-16 12:58:55 +0530

    Changed the css

--- src/Pages/User/Profile.js
+++ src/Pages/User/Profile.js
@@ -24,11 +24,11 @@ const Profile = () => {
     <Layout>
       <div className="min-h-[90vh] flex items-center justify-center">
         <div className="my-10 flex flex-col gap-4 rounded-lg p-4 text-white w-80 shadow-[0_0_10px_black]">
-          <img
+          {/* <img
             className="w-40 m-auto rounded-full border border-black"
             src={userData?.avatar?.secure_url}
             alt="user profile image"
-          />
+          /> */}
 
           <h3 className="text-xl font-semibold text-center capitalize">
             {userData.fullName}
@@ -36,15 +36,11 @@ const Profile = () => {
 
           <div className="grid grid-cols-2">
             <p>Email :</p>
-            <p>{userData?.email}</p>
+            <p>{userData?.email}</p><br></br>
             <p>Role :</p>
             <p>{userData?.role}</p>
-            <p>Subscription :</p>
-            <p>
-              {userData?.subscription?.status === "active"
-                ? "Active"
-                : "Inactive"}
-            </p>
+            <br></br>
+         
           </div>
 
           {/* button to change the password */}
@@ -55,8 +51,8 @@ const Profile = () => {
                   ? "/denied"
                   : "/changepassword"
               }
-              className="w-1/2 bg-yellow-600 hover:bg-yellow-700 transition-all ease-in-out duration-300 rounded-sm py-2 font-semibold cursor-pointer text-center"
-            >
+              className="w-1/0.1 border border-yellow-600 hover:border-yellow-500 transition-all ease-in-out duration-300 rounded-sm py-2 font-semibold cursor-pointer text-center"
+              >
               <button>Change Password</button>
             </Link>
 
