8ebc90ee49ed117f9f96e34a96ac224e684b6cf3
Kunal Kumar <kunal23kmr@gmail.com>
2024-02-16 21:49:56 +0530

    Contact from done.

--- src/USER/Contact/Contact.js
+++ src/USER/Contact/Contact.js
@@ -1,78 +1,116 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Contact.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faMapMarkerAlt, faUser } from '@fortawesome/free-solid-svg-icons';
-import { useLocation } from 'react-router-dom';
+import axiosInstance from '../../Helper/axiosInstance';
+import toast from 'react-hot-toast';
 
 const ContactSection = () => {
-  const location = useLocation();
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    console.log(name);
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(formData);
+
+    try {
+      const url = `/contact`
+      const res = await axiosInstance.post(url, formData);
+      console.log(res.data);
+      if (res?.data?.success) {
+        toast.success(res.data.message)
+      } else {
+        toast.error(res?.data?.message);
+      }
+      setFormData({
+        name: "",
+        email: "",
+        subject: "",
+        message: "",
+      })
+
+    } catch (err) {
+      console.log(err);
+      toast.error('Connection Error');
+    }
+  }
+
   return (
-    <div>
-      {(location.pathname === '/Contacts') && <div style={{ minHeight: '5rem' }}></div>}
-      <section style={{ backgroundColor: 'aliceblue', padding: '30px 0px 30px' }}>
-        <div className="contact" id="contact">
-          <div className="max-width">
-            <h2 className="title" style={{ textAlign: 'center', color: '#070707', marginBottom: '30px' }}>Contact Us</h2>
+    <section style={{ backgroundColor: 'aliceblue', padding: '30px 0px 30px' }}>
+      <div className="contact" id="contact">
+        <div className="max-width">
+          <h2 className="title" style={{ textAlign: 'center', color: '#070707', marginBottom: '30px' }}>Contact Us</h2>
 
-            <div className="contact-content">
-              <div className="column left">
-                <div className="text">Get in Touch</div>
-                <div className="icons">
-                  <div className="row">
+          <div className="contact-content">
+            <div className="column left">
+              <div className="text">Get in Touch</div>
+              <div className="icons">
+                <div className="row">
 
 
 
-                    <FontAwesomeIcon icon={faUser} />
-                    <div className="info">
-                      {/* <div className="head">Name</div> */}
-                      <div className="sub-title">Utkansh Management Team</div>
-                    </div>
+                  <FontAwesomeIcon icon={faUser} />
+                  <div className="info">
+                    <div className="head">Name</div>
+                    <div className="sub-title">Abhishek Kumar</div>
                   </div>
-                  <div className="row">
-                    <FontAwesomeIcon icon={faMapMarkerAlt} />
-                    <div className="info">
-                      <div className="head">Address</div>
-                      <div className="sub-title">NIT Jalandhar
-                      </div>
-                    </div>
+                </div>
+                <div className="row">
+                  <FontAwesomeIcon icon={faMapMarkerAlt} />
+                  <div className="info">
+                    <div className="head">Address</div>
+                    <div className="sub-title">Samastipur Bihar</div>
                   </div>
-                  <div className="row">
-                    <FontAwesomeIcon icon={faEnvelope} />
-                    <div className="info">
-                      <div className="head">Email</div>
-                      <div className="sub-title">utkansh@nitj.ac.in</div>
-                    </div>
+                </div>
+                <div className="row">
+                  <FontAwesomeIcon icon={faEnvelope} />
+                  <div className="info">
+                    <div className="head">Email</div>
+                    <div className="sub-title">abhishek110803@gmail.com</div>
                   </div>
                 </div>
               </div>
-              <div className="column right">
-                <div className="text">Message me</div>
-                <form action="#" className='contactform'>
-                  <div className="fields">
-                    <div className="field namee">
-                      <input type="text" placeholder="Name" className='textarae' required />
-                    </div>
+            </div>
 
-                    <div className="field email">
-                      <input type="email" placeholder="Email" className='textarae' required />
-                    </div>
+
+            <div className="column right">
+              <div className="text">Message me</div>
+              <form className='contactform' onSubmit={handleSubmit}>
+                <div className="fields">
+                  <div className="field namee">
+                    <input type="text" placeholder="Name" name='name' className='textarae' required onChange={handleChange} value={formData.name} />
                   </div>
-                  <div className="field">
-                    <input type="text" placeholder="Subject" className='textarae' required />
+
+                  <div className="field email">
+                    <input type="email" placeholder="Email" name='email' className='textarae' required onChange={handleChange} value={formData.email} />
                   </div>
-                  <div className="field textarea textarat">
-                    <textarea cols="30" rows="10" placeholder="Message.." className='textarae' required></textarea>
-                  </div>
-                  <div className="button-area">
-                    <button type="submit">Send message</button>
-                  </div>
-                </form>
-              </div>
+                </div>
+                <div className="field">
+                  <input type="text" placeholder="Subject" name='subject' className='textarae' required onChange={handleChange} value={formData.subject} />
+                </div>
+                <div className="field textarea textarat">
+                  <textarea cols="30" rows="10" placeholder="Message.." name='message' className='textarae' required onChange={handleChange} value={formData.message} ></textarea>
+                </div>
+                <div className="button-area">
+                  <button type="submit">Send message</button>
+                </div>
+              </form>
             </div>
           </div>
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
   );
 };
 
