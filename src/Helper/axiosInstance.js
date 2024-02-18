03b174d1a77920d4e3b5280d09fdfe4ffc6ea60d
Kunal Kumar <kunal23kmr@gmail.com>
2024-02-18 14:00:45 +0530

    local check

--- src/Helper/axiosInstance.js
+++ src/Helper/axiosInstance.js
@@ -11,8 +11,8 @@
 
 import axios from "axios";
 
-// const BASE_URL = "http://localhost:5000/api/v1";
-const BASE_URL = "https://utk-b-24.onrender.com/api/v1";
+const BASE_URL = "http://localhost:5000/api/v1";
+// const BASE_URL = "https://utk-b-24.onrender.com/api/v1";
 
 const axiosInstance = axios.create();
 
