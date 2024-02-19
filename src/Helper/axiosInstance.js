054ecc1d1ebd7454a71c6022d49ae6e939793625
Kunal Kumar <kunal23kmr@gmail.com>
2024-02-19 22:06:08 +0530

    testing

--- src/Helper/axiosInstance.js
+++ src/Helper/axiosInstance.js
@@ -1,20 +1,32 @@
-// import axios from "axios";
-
-// const BASE_URL = "http://localhost:5000/api/v1/";
-
-// const axiosInstance = axios.create();
-
-// axiosInstance.defaults.baseURL = BASE_URL;
-// axiosInstance.defaults.withCredentials = true;
-
-// export default axiosInstance;
-
 import axios from "axios";
+import Cookies from 'js-cookie';
 
 const BASE_URL = "http://localhost:5000/api/v1/";
 // const BASE_URL = "https://utk-b-24.onrender.com/api/v1/";
 
-const axiosInstance = axios.create();
+
+// Create an instance of axios with custom configuration
+const axiosInstance = axios.create({
+    baseURL: BASE_URL,
+    timeout: 50000,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+// Add a request interceptor to include JWT token in the headers
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const token = Cookies.get('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 axiosInstance.defaults.baseURL = BASE_URL;
 axiosInstance.defaults.withCredentials = true;
