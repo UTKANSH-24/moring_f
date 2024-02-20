c63683a9dbf907c1dc408045412dca9967639ed2
Kunal Kumar <kunal23kmr@gmail.com>
2024-02-20 14:18:39 +0530

    try2

--- src/Helper/axiosInstance.js
+++ src/Helper/axiosInstance.js
@@ -9,7 +9,7 @@ const axiosInstance = axios.create({
     headers: {
         'Content-Type': 'application/json',
     },
-    // withCredentials:true,
+    withCredentials:true,
 });
 
 // Add a request interceptor to include JWT token in the headers
