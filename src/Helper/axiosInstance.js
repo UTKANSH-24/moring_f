ed746cb75df81ab3b30f44aa4175b3dfdc2d0959
Kunal Kumar <kunal23kmr@gmail.com>
2024-02-20 15:02:15 +0530

    axios

--- src/Helper/axiosInstance.js
+++ src/Helper/axiosInstance.js
@@ -4,7 +4,7 @@ import Cookies from 'js-cookie';
 
 // Create an instance of axios with custom configuration
 const axiosInstance = axios.create({
-    baseURL: process.env.BASE_URL,
+    baseURL: 'https://utkb.vercel.app/api/v1/',
     timeout: 50000,
     headers: {
         'Content-Type': 'application/json',
@@ -15,7 +15,7 @@ const axiosInstance = axios.create({
 // Add a request interceptor to include JWT token in the headers
 axiosInstance.interceptors.request.use(
     (config) => {
-        console.log(Cookies);
+        console.log(config);
         const token = Cookies.get('token');
         if (token) {
             config.headers.Authorization = `Bearer ${token}`;
@@ -27,7 +27,6 @@ axiosInstance.interceptors.request.use(
     }
 );
 
-axiosInstance.defaults.baseURL = process.env.BASE_URL;
 axiosInstance.defaults.withCredentials = true;
 
 export default axiosInstance;
