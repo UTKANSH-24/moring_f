cc0ec303477b5196f7c180a897fddf72cdc7e2f9
Kunal Kumar <kunal23kmr@gmail.com>
2024-02-20 21:45:43 +0530

    shree ganeshay namah

--- src/Helper/axiosInstance.js
+++ src/Helper/axiosInstance.js
@@ -4,7 +4,7 @@ import Cookies from 'js-cookie';
 
 // Create an instance of axios with custom configuration
 const axiosInstance = axios.create({
-    baseURL: 'https://utkb.vercel.app/api/v1/',
+    baseURL: 'https://utkb.vercel.app/api/v1',
     // baseURL: 'http://localhost:5000/api/v1/',
     // baseURL: "https://utkansh2024backend.onrender.com/api/v1/",
     timeout: 50000,
